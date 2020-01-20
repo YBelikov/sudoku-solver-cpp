45b7bdd1cf60ebaf1f4e7b1368edc66ec8df80bc
YBelikov <47750806+YBelikov@users.noreply.github.com>
2020-01-21 00:32:01 +0300

    Implement class for game field

--- FieldGrid.h
+++ FieldGrid.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include <vector>
+
+template<typename ValueType>
+
+class FieldGrid {
+public:
+	FieldGrid();
+	FieldGrid(int, int);
+	int numberOfRows() const;
+	int numberOfColumns() const;
+	void resize(int, int);
+	ValueType get(int, int) const;
+	ValueType operator[][](int, int);
+
+private:
+	std::vector<std::vector<ValueType>> grid;
+};
